43675c54b1d73ca0ed9c1af08eda266275ed3f23
Tor <arbiter99@live.com>
2020-11-24 16:39:28 +0100

    jenkins test commit 2

--- src/main/java/com/cicd/calculator/CalculatorApplication.java
+++ src/main/java/com/cicd/calculator/CalculatorApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
-    //Jenkins test commit message
+    //Jenkins test commit message 2
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
     }
